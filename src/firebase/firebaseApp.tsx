50d7105aeaeda2ae33e521804e628c0054ed89be
JackPadalino <pada0867@gmail.com>
2024-06-18 11:41:53 -0400

    set up config file for firebase and updated ignore file

---
+++ src/firebase/firebaseApp.tsx
@@ -0,0 +1,25 @@
+import { initializeApp, getApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase (if not already initialized)
+const app = initializeApp(firebaseConfig);
+
+// Get Firebase services (only after initialization)
+const auth = getAuth(app);
+const firestore = getFirestore(app);
+const storage = getStorage(app); // Get Storage after app initialization
+
+export { app, auth, firestore, storage };
