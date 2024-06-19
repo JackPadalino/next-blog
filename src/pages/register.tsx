db8cfe486c73f9c22441402c746d9a1a33e01512
JackPadalino <pada0867@gmail.com>
2024-06-19 14:50:30 -0400

    added registration form

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/firebaseApp";
 import {
   Box,
@@ -16,21 +15,25 @@ import {
 } from "@chakra-ui/react";
 import styles from "@/styles/Login.module.css";
 
-interface LoginType {
+interface RegisterType {
   email: string;
   password: string;
+  confirmPassword: string;
 }
 
-const Login = () => {
+const Register = () => {
   const user = auth.currentUser; // currently signed in user?
   const router = useRouter();
 
   //Form state and functions
   const [show, setShow] = useState<boolean>(false);
+  const [registerError, setRegisterError] = useState<string>("");
+
   const handleClick = () => setShow(!show);
-  const [formData, setFormData] = useState<LoginType>({
+  const [formData, setFormData] = useState<RegisterType>({
     email: "",
     password: "",
+    confirmPassword: "",
   });
 
   // Event handler for input change
@@ -43,32 +46,42 @@ const Login = () => {
   };
 
   // login handler function
-  const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
+  const handleRegister = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    try {
-      // const response = await signInWithEmailAndPassword(
-      //   auth,
-      //   formData.email,
-      //   formData.password
-      // );
-      // const user = response.user;
-      await signInWithEmailAndPassword(auth, formData.email, formData.password);
-      router.push("/");
-    } catch (error: any) {
-      // The 'any' type here captures any kind of error thrown
-      const errorCode = error.code;
-      const errorMessage = error.message;
-      console.error(`Error signing in (${errorCode}): ${errorMessage}`);
+    if (formData.password === formData.confirmPassword) {
+      try {
+        // const response = await signInWithEmailAndPassword(
+        //   auth,
+        //   formData.email,
+        //   formData.password
+        // );
+        // const user = response.user;
+        await createUserWithEmailAndPassword(
+          auth,
+          formData.email,
+          formData.password
+        );
+        router.push("/");
+      } catch (error: any) {
+        // The 'any' type here captures any kind of error thrown
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        if (errorCode === "auth/email-already-in-use") {
+          setRegisterError("An account with this email already exists.");
+        }
+      }
+    } else {
+      setRegisterError("Please make sure passwords match.");
     }
   };
 
   return (
     <Box className={`${styles.loginContainer}`}>
-      <Heading>Login page</Heading>
+      <Heading>Register</Heading>
       {user ? (
         <Text>You are already signed in.</Text>
       ) : (
-        <form className={`${styles.loginForm}`} onSubmit={handleLogin}>
+        <form className={`${styles.loginForm}`} onSubmit={handleRegister}>
           <FormControl id="email" isRequired>
             <Input
               type="email"
@@ -97,19 +110,25 @@ const Login = () => {
               </InputRightElement>
             </InputGroup>
           </FormControl>
+          <FormControl id="confirmPassword" isRequired>
+            <Input
+              type={show ? "text" : "password"}
+              placeholder="Confirm password"
+              pr="4.5rem"
+              borderColor="grey"
+              focusBorderColor="dark-grey"
+              value={formData.confirmPassword}
+              onChange={handleInputChange}
+            />
+          </FormControl>
           <Button type="submit" colorScheme="teal">
-            Login
+            Register
           </Button>
-          <Text>
-            Dont have an account?{" "}
-            <Link className={`${styles.registerLink}`} href="/register">
-              Register now
-            </Link>
-          </Text>
+          {registerError !== "" && <Text>{registerError}</Text>}
         </form>
       )}
     </Box>
   );
 };
 
-export default Login;
+export default Register;
