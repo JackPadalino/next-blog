b745aabc868e97e1bb1da99e00c1345b065ed91d
JackPadalino <pada0867@gmail.com>
2024-06-20 15:00:34 -0400

    updated posts page - added logic to add post to Firestore db

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, ChangeEvent, FormEvent } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
-import { auth } from "../firebase/firebaseApp";
+import { auth, db } from "../firebase/firebaseApp";
+import { collection, doc, addDoc } from "firebase/firestore";
 import {
   Box,
   Text,
+  Textarea,
   Heading,
   Button,
   Input,
@@ -13,12 +15,69 @@ import {
   InputLeftAddon,
   InputRightElement,
 } from "@chakra-ui/react";
-import styles from "@/styles/Login.module.css";
+import styles from "@/styles/Posts.module.css";
+
+interface PostType {
+  userId: string;
+  content: string;
+}
 
 const Posts = () => {
+  const user = auth.currentUser; // currently signed in user?
+  const [formData, setFormData] = useState<PostType>({
+    userId: "",
+    content: "",
+  });
+
+  // Event handler for input change
+  const handleInputChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    const { id, value } = e.target;
+    setFormData({
+      ...formData,
+      [id]: value,
+    });
+  };
+
+  const handlePost = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // Add a new post with a generated id
+    const data = {
+      userId: user?.uid,
+      content: formData.content,
+    };
+    try {
+      await addDoc(collection(db, "posts"), data);
+      setFormData({
+        userId: "",
+        content: "",
+      });
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+
   return (
     <Box className={`${styles.postsContainer}`}>
       <Heading>Posts</Heading>
+      {user && (
+        <form className={`${styles.postForm}`} onSubmit={handlePost}>
+          <FormControl id="content" isRequired>
+            <Textarea
+              // isRequired
+              value={formData.content}
+              onChange={handleInputChange}
+              placeholder="Say something nice..."
+              borderColor="grey"
+              focusBorderColor="dark-grey"
+              //   size="sm"
+            />
+          </FormControl>
+
+          <Button type="submit" colorScheme="teal">
+            Post
+          </Button>
+        </form>
+      )}
     </Box>
   );
 };
