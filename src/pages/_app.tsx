9145e3423559232b16b85e6bfde9073b7e17ea74
JackPadalino <pada0867@gmail.com>
2024-06-22 12:46:05 -0400

    updated comments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,14 @@ import FetchData from "@/components/fetchData";
 import Layout from "../components/layout";
 import "@/styles/globals.css";
 
+// imports for fetching data
+// import { useEffect } from "react";
+// import { db } from "../firebase/firebaseApp";
+// import { collection, getDocs } from "firebase/firestore";
+// import { useRecoilState } from "recoil";
+// import { postsState } from "../recoil/postsAtom";
+// import { PostType } from "@/types/appTypes";
+
 /**
  * Root of application
  * FetchData is a component that is fetching data from Firebase DB
@@ -13,6 +21,21 @@ export default function App({ Component, pageProps }: AppProps) {
   // Tried to put logic for fetching Firestore data here inside this
   // app component, but this does not work. The logic must be wrapped
   // inside the <RecoilRoot> component.
+  // const [recoilPostsState, setRecoilPostsState] = useRecoilState(postsState);
+  // const fetchPosts = async () => {
+  //   console.log("fetching posts");
+  //   try {
+  //     const postsSnapshot = await getDocs(collection(db, "posts"));
+  //     const xPosts = postsSnapshot.docs.map((doc) => doc.data() as PostType);
+  //     setRecoilPostsState((prevState) => ({
+  //       ...prevState,
+  //       posts: xPosts,
+  //     }));
+  //   } catch (error: any) {
+  //     console.log(error);
+  //   }
+  // };
+
   return (
     <RecoilRoot>
       <FetchData />
