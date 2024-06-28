d24e44048c6ab4a4a65380ef64040910aa145689
JackPadalino <pada0867@gmail.com>
2024-06-28 19:09:15 -0400

    removed qdrant - testing google vector search extension for firestore

--- src/qdrant/qdrant.tsx
+++
@@ -1,8 +0,0 @@
-import { QdrantClient } from "@qdrant/js-client-rest";
-
-const qdrant = new QdrantClient({
-  url: process.env.NEXT_PUBLIC_QDRANT_DB_URL,
-  apiKey: process.env.NEXT_PUBLIC_QDRANT_API_KEY,
-});
-
-export default qdrant;
