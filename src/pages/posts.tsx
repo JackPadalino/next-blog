772e0052260b0dd85bd39660a5b7cde65f26c093
JackPadalino <pada0867@gmail.com>
2024-06-28 21:14:19 -0400

    have vector search working - able to retrieve an array of ids from posts collection that best match the users query

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -6,10 +6,17 @@ import React, {
   SyntheticEvent,
 } from "react";
 
-import { auth, db } from "../firebase/firebaseApp";
-import { collection, doc, addDoc, getDocs } from "firebase/firestore";
+import { auth, db, functions } from "../firebase/firebaseApp";
+import {
+  collection,
+  doc,
+  addDoc,
+  getDocs,
+  getDoc,
+  query,
+} from "firebase/firestore";
 
-import gemini from "@/gemini/geminiConfig";
+// import gemini from "@/gemini/geminiConfig";
 
 import { postsState } from "@/recoil/postsAtom";
 import { useRecoilState } from "recoil";
@@ -34,6 +41,9 @@ import {
 import { PostType } from "../types/appTypes"; // importing type
 import styles from "@/styles/Posts.module.css";
 
+import { httpsCallable } from "firebase/functions";
+import { signInAnonymously } from "firebase/auth";
+
 const Posts = () => {
   const user = auth.currentUser; // currently signed in user?
   const [recoilPostsState, setRecoilPostsState] = useRecoilState(postsState);
@@ -43,13 +53,12 @@ const Posts = () => {
     userId: "",
     title: "",
     content: "",
-    vectorEmbedding: [],
+    // embedding: [],
   });
   const [searchFormQuery, setSearchFormQuery] = useState<string>("");
 
   // fetching posts from Firebase
   const fetchPosts = async () => {
-    console.log("fetching posts");
     try {
       const postsSnapshot = await getDocs(collection(db, "posts"));
       // have to explicitly say that each document coming from Firebase DB is a 'PostType'
@@ -87,13 +96,13 @@ const Posts = () => {
     if (!user) {
       return;
     }
-    // generate vector embedding data (to be used for semantic search)
-    // getting an error about embedding the userId (number) here
-    // for now we will only pass the title and content from the form to Gemini
-    const vectorString = `title:${postFormData.title},content:${postFormData.content}`;
     try {
-      const response = await gemini.embedContent(vectorString);
-      const embedding = response.embedding.values;
+      // generate vector embedding data (to be used for semantic search)
+      // getting an error about embedding the userId (number) here
+      // for now we will only pass the title and content from the form to Gemini
+      // const vectorString = `title:${postFormData.title},content:${postFormData.content}`;
+      // const response = await gemini.embedContent(vectorString);
+      // const embedding = response.embedding.values;
 
       // Add a new post with a generated id
       // and embedding data from gemini
@@ -101,7 +110,7 @@ const Posts = () => {
         userId: user?.uid,
         title: postFormData.title,
         content: postFormData.content,
-        vectorEmbedding: embedding,
+        // embedding: embedding,
       };
 
       await addDoc(collection(db, "posts"), data);
@@ -110,7 +119,7 @@ const Posts = () => {
         userId: "",
         title: "",
         content: "",
-        vectorEmbedding: [],
+        // embedding: [],
       });
     } catch (error: any) {
       console.log(error);
@@ -124,18 +133,36 @@ const Posts = () => {
   const handleSearch = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     try {
-      const response = await gemini.embedContent(searchFormQuery);
-      const embedding = response.embedding.values;
-      console.log(embedding);
+      // const response = await gemini.embedContent(searchFormQuery);
+      // const embedding = response.embedding.values;
+      // console.log(embedding);
+
+      // sign in anonymously (enabled to allow users to search)
+      signInAnonymously(auth)
+        .then(() => {
+          const queryCallable = httpsCallable(
+            functions,
+            "ext-firestore-vector-search-jn7i-queryCallable"
+          );
+          // perform the search and limit the results to 10
+          queryCallable({ query: searchFormQuery, limit: 10 })
+            .then((result) => {
+              // display the results - an array of ids that best match our query
+              // closest matches first
+              console.log(result.data);
+            })
+            .catch((error) => {
+              console.error("Error querying function:", error);
+            });
+        })
+        .catch((error) => {
+          console.error("Error signing in anonymously:", error);
+        });
     } catch (error: any) {
       console.log(error);
     }
     setSearchFormQuery("");
   };
-
-  //   useEffect(() => {
-  //     fetchPosts();
-  //   }, []);
 
   return (
     <Box className={`${styles.postsMainContainer}`}>
