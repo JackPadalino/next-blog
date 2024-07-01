7fb9ffa32f69f6ccea1442eda1fd4fdb866b4e00
JackPadalino <pada0867@gmail.com>
2024-06-30 20:33:45 -0400

    displaying search results - pulling search results from recoil store

--- src/pages/results.tsx
+++ src/pages/results.tsx
@@ -4,26 +4,33 @@ import { useRecoilState } from "recoil";
 import { searchResultsState } from "../recoil/searchAtom";
 import { SearchResultType } from "@/types/appTypes";
 
-import {
-  Box,
-  Text,
-  Heading,
-  Card,
-  CardHeader,
-  CardBody,
-  CardFooter,
-} from "@chakra-ui/react";
+import { Box, Text, Heading, Card, CardBody } from "@chakra-ui/react";
+
+import styles from "@/styles/Results.module.css";
 
 const Results = () => {
   const [recoilSearchResultsState, setRecoilSearchResultsState] =
     useRecoilState(searchResultsState);
 
-  // pulling search results from Recoil - sent up from navbar upon user search
-  console.log(recoilSearchResultsState);
-
   return (
-    <Box>
+    <Box className={styles.resultsMainContainer}>
       <Heading>Search results</Heading>
+      {recoilSearchResultsState.searchResults.length ? (
+        <Box className={`${styles.resultsCardsContainer}`}>
+          {recoilSearchResultsState.searchResults.map(
+            (searchResult: SearchResultType, index: number) => (
+              <Card key={index} className={`${styles.resultCard}`}>
+                <CardBody>
+                  <Heading>{searchResult.title}</Heading>
+                  <Text>{searchResult.content}</Text>
+                </CardBody>
+              </Card>
+            )
+          )}
+        </Box>
+      ) : (
+        <Text>Sorry, we didn't find anything related to your search.</Text>
+      )}
     </Box>
   );
 };
