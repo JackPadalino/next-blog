4a476037e64af85a557b52994ac8b739ccc4bf9b
JackPadalino <pada0867@gmail.com>
2024-07-03 12:45:49 -0400

    change path command

--- src/cloud/cloudConfig.ts
+++ src/cloud/cloudConfig.ts
@@ -4,7 +4,7 @@ const path = require("path");
 // setting the env variable to store the file path to the
 // service account credentials.json file
 // do this to set up the firestore client
-process.env.GOOGLE_APPLICATION_CREDENTIALS = path.resolve(
+process.env.GOOGLE_APPLICATION_CREDENTIALS = path.join(
   process.cwd(),
   "credentials.json"
 );
