6827c2e07321d418f7d1cfc6bef67e7befea4250
JackPadalino <pada0867@gmail.com>
2024-07-10 12:27:16 -0400

    added logic to add new post with vector embeddings to firestore db

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -1,32 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import gemini from "@/gemini/geminiConfig";
-import {
-  Firestore,
-  FieldValue,
-  VectorQuery,
-  VectorQuerySnapshot,
-} from "@google-cloud/firestore";
+import { FieldValue } from "@google-cloud/firestore";
 import firestoreClient from "@/cloud/cloudConfig";
-import { db } from "@/firebase/firebaseApp";
-import { collection, getDocs } from "firebase/firestore";
-// import { PostType } from "@/types/appTypes";
 
-/** Need to add logic to add new post to firestore db */
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
-    let response = req.body;
-    const geminiResponse = await gemini.embedContent(response.content);
+    let newPost = req.body;
+    // generate text embedding for new post with Gemini embeddings model
+    const geminiResponse = await gemini.embedContent(newPost.content);
     let postEmbedding = geminiResponse.embedding.values;
     postEmbedding = FieldValue.vector(postEmbedding);
-    response.embedding = postEmbedding;
-    res.status(200).json(response);
-    // const querySnapshot = await getDocs(collection(db, "posts"));
-    // querySnapshot.forEach((doc) => {
-    //   // doc.data() is never undefined for query doc snapshots
-    //   console.log(doc.id, " => ", doc.data());
-    // });
-    // res.status(200).json("ok");
+    newPost.embedding = postEmbedding;
+
+    // add a new document with a generated id.
+    const postsRef = firestoreClient.collection("posts");
+    const newPostRef = await postsRef.add(newPost);
+    res.status(200).json({ message: "Post successful", postId: newPostRef.id });
   } catch (error) {
     console.error("Error making post:", error);
     res.status(500).json({ message: "Internal Server Error", embedding: [] });
