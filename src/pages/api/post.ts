a0f09a5ed2030b911628d11331e70e7506f1a06c
JackPadalino <pada0867@gmail.com>
2024-07-11 13:13:26 -0400

    added logic to endpoint to calculate magnitude for newly created post

--- src/pages/api/post.ts
+++ src/pages/api/post.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import gemini from "@/gemini/geminiConfig";
-import { FieldValue } from "@google-cloud/firestore";
+// import { FieldValue } from "@google-cloud/firestore";
 import firestoreClient from "@/cloud/cloudConfig";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
@@ -10,8 +10,23 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
     // generate text embedding for new post with Gemini embeddings model
     const geminiResponse = await gemini.embedContent(newPost.content);
     let postEmbedding = geminiResponse.embedding.values;
-    postEmbedding = FieldValue.vector(postEmbedding);
+    /**
+    have to comment this out - to perform the cosine similarity
+    calculation in the search api endpoint the embedding must
+    be a plain array - not a FieldValue type
+    */
+    // postEmbedding = FieldValue.vector(postEmbedding);
+
+    // calculating embeddings array magnitude
+    // sum of the squares of each component
+    const sumOfSquares = postEmbedding.reduce(
+      (sum: number, value: number) => sum + value * value,
+      0
+    );
+    // square root of the sum of squares
+    const embeddingMag = Math.sqrt(sumOfSquares);
     newPost.embedding = postEmbedding;
+    newPost.embeddingMag = embeddingMag;
 
     // add a new document with a generated id.
     const postsRef = firestoreClient.collection("posts");
